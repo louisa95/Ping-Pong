bbd2f63d25b8b46895a30e6ca872524de6c1563c
louisa konchellah <louisakonchellah@gmail.com>
2018-07-13 16:39:23 +0300

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
-for (var i=1; i < 101; i++){
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
+$ (document).ready(function(){
+  $("form#myForm").submit(function(event){
+    event.preventDefault()
+  var userNumber = parseInt($("input#userInput").val())
+console.log(userNumber);
+
+
+for (var i=1; i < userNumber; i++){
+  var result=""
+    if (i % 15 == 0) console.log("PingPongP");
+    else if (i % 3 == 0) console.log("Ping");
+    else if (i % 5 == 0) console.log("Pong");
     else console.log(i);
+    $("p#display").append(result+"</br>")
 }
+
+  });
+});
