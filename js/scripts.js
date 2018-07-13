2763f8fc642a403d9474bb3067c6634cf5292a46
louisa konchellah <louisakonchellah@gmail.com>
2018-07-13 17:44:50 +0300

    edit READ.me

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ $ (document).ready(function(){
   var userNumber = parseInt($("input#userInput").val());
 console.log(userNumber);
 
-for (var i=1; i < userNumber; i++){
+for (var i=1; i <= userNumber; i++){
   var result = "";
-    if (i % 15 == 0) result = "PingPong";
-    if (i % 3 == 0) result = "Ping";
-    if (i % 5 == 0) result="Pong";
+
+    if (i % 3 === 0) result = "Ping";
+    if (i % 5 === 0) result += "Pong";
     if (i % 5 != 0 && i % 3 != 0) result = i;
 
     $("p#display").append(result+"</br>")
