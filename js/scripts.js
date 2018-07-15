c8709e4de603ca35842308e3e3c119768d201b4b
louisa konchellah <louisakonchellah@gmail.com>
2018-07-15 19:15:49 +0300

    deployment

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$ (document).ready(function(){
-  $("form#myForm").submit(function(event){
+$(document).ready(function() {
+  $("form#myForm").submit(function(event) {
     event.preventDefault();
-  var userNumber = parseInt($("input#userInput").val());
-console.log(userNumber);
+    $("p#display").html('');
 
-for (var i=1; i <= userNumber; i++){
-  var result = "";
+    var userNumber = parseInt($("input#userInput").val());
+    for (var i = 1; i <= userNumber; i++) {
+      var result = "";
 
-    if (i % 3 === 0) result = "Ping";
-    if (i % 5 === 0) result += "Pong";
-    if (i % 5 != 0 && i % 3 != 0) result = i;
+      if (i % 3 === 0) result = "Ping";
+      if (i % 5 === 0) result += "Pong";
+      if (i % 5 != 0 && i % 3 != 0) result = i;
 
-    $("p#display").append(result+"</br>")
-}
+      $("p#display").append(result + "</br>");
+    };
 
-});
+  });
 });
